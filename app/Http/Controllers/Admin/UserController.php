a8b901c4b491535ff893772f07a4c799311d0465
wolf <ahmedragabyasin2020@gmail.com>
2021-12-18 18:48:00 +0200

    edit and delete by ajax yagra

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use DataTables;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -21,8 +22,8 @@ class UserController extends Controller
             return Datatables::of($users)
                     ->addIndexColumn()
                     ->addColumn('action', function($row){
-                    $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Edit" class="edit btn btn-primary btn-sm editBook">Edit</a>';
-                    $btn = $btn.' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Delete" class="btn btn-danger btn-sm deleteBook">Delete</a>';
+                    $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Edit" class="edit btn btn-primary btn-sm editUser">Edit</a>';
+                    $btn = $btn.' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Delete" class="btn btn-danger btn-sm deleteUser">Delete</a>';
                     return $btn;
                     })
                     ->rawColumns(['action'])
@@ -31,25 +32,18 @@ class UserController extends Controller
         return view('Admin.user.index', compact('users'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        User::updateOrCreate(
+        [
+        'id' => $request->user_id
+        ],
+        [
+         'name' => $request->name,
+         'email' => $request->email,
+         'password' => Hash::make($request->password),
+        ]);
+        return response()->json(['success'=>'user saved successfully.']);
     }
 
     /**
@@ -63,37 +57,14 @@ class UserController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-        //
+        return User::findOrFail($id);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        User::find($id)->delete();
+        return response()->json(['success'=>'User deleted successfully.']);
     }
 }
--- resources/views/Admin/user/index.blade.php
+++ resources/views/Admin/user/index.blade.php
@@ -12,9 +12,12 @@
         <div class="card m-b-30">
             <div class="card-body">
                 <h4 class="mt-0 header-title">{{ __('user table') }}</h4>
-                <div class="col-md-6 mb-3">
+                {{-- <div class="col-md-6 mb-3">
                     <a href="#"  class="btn btn-primary "><i class="fa fa-user-circle"></i> {{ __('Add User') }}</a>
-                </div>
+                </div> --}}
+                <button type="button" class="btn btn-primary mb-3" data-toggle="modal" data-target="#adduser">
+                    <i class="fa fa-user-circle"></i> {{ __('Add User') }}
+                </button>
                 <div class="table-responsive">
                     <table id="datatable" class="table table-bordered dt-responsive nowrap data-table" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
                         <thead>
@@ -22,6 +25,7 @@
                                 <th>#</th>
                                 <th>{{ __('name') }}</th>
                                 <th>{{ __('email') }}</th>
+                                {{-- <th>{{ __('password') }}</th> --}}
                                 <th>{{ __('action') }}</th>
                             </tr>
                         </thead>
@@ -30,6 +34,52 @@
                         </tbody>
                     </table>
                 </div>
+
+                                    <div class="modal fade " id="adduser" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                            <h5 class="modal-title" id="modalHeading">Create User</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                            </div>
+
+                                            @include('alerts.create-modal')
+                                            <div class="modal-body">
+                                                <div class="alert-danger"></div>
+                                                    <form id="userForm" name="userForm" class="form-horizontal ">
+                                                    <input type="hidden" name="user_id" id="user_id">
+                                                        <div class="form-group">
+                                                            <label for="name" class="col-sm-2 control-label">Name</label>
+                                                            <div class="col-sm-12">
+                                                                <input type="text" id="name" name="name" class="form-control"  placeholder="Enter name" maxlength="50" required="">
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="form-group">
+                                                            <label class="col-sm-2 control-label">Email</label>
+                                                            <div class="col-sm-12">
+                                                                <input type="email" id="email" name="email" class="form-control" placeholder="Enter email">
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group">
+                                                            <label class="col-sm-2 control-label">Password</label>
+                                                            <div class="col-sm-12">
+                                                                <input type="password" id="password" name="password" class="form-control" placeholder="Enter password">
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                            <button type="button" id="saveBtn" name="saveBtn" class="btn btn-primary">Save changes</button>
+                                                        </div>
+                                                    </form>
+                                                </div>
+
+                                            </div>
+                                            </div>
+                                    </div>
             </div>
         </div>
     </div>
@@ -41,6 +91,7 @@
 <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+<script src="{{ asset('js/alerts.js') }}"></script>
 <script>
 $(function () {
     $.ajaxSetup({
@@ -56,8 +107,83 @@ $(function () {
         {data: 'DT_RowIndex', name: 'DT_RowIndex'},
         {data: 'name', name: 'name'},
         {data: 'email', name: 'email'},
+        // {data: 'name', name: 'password'},
         {data: 'action', name: 'action', orderable: false, searchable: false},
        ]
+    });
+
+    $('#saveBtn').click(function (e) {
+        e.preventDefault();
+        $(this).html('Saving...');
+
+        // var author = $("#author").val();
+        // console.log(author);
+        $.ajax({
+          data: $('#userForm').serialize(),
+          url: "{{ route('user.store') }}",
+          type: "POST",
+          dataType: 'json',
+          success: function (data) {
+
+            var show = $('.modal').hasClass('show');
+            if(show)
+            {
+              $(this).on("click", function () {
+                // alertSuccess("Book added successfully.");
+                console.log("clicked on modal");
+              });
+            }
+            else
+            {
+                console.log("modal is closed");
+            }
+
+            // alertSuccess("Book added successfully.");
+
+              $('#bookForm').trigger("reset");
+            //   $('#exampleModal').modal('hide');
+            //   $(".modal-backdrop").remove();
+              table.draw();
+
+          },
+          error: function (data) {
+
+            // var errors = data.responseText;
+            // console.log( errors );
+        }
+
+            //   $('#saveBtn').html('Save Changes');
+    });
+});
+// Edit call
+$('body').on('click', '.editUser', function () {
+      var user_id = $(this).data('id');
+      $.get("{{ url('user/') }}" +'/' + user_id +'/edit', function (data) {
+          $('#modelHeading').html("Edit User");
+          $('#saveBtn').val("edit-user");
+          $('#adduser').modal('show');
+          $('#user_id').val(data.id);
+          $('#name').val(data.name);
+          $('#email').val(data.email);
+      })
+   });
+    ///Delete Call
+    $('body').on('click', '.deleteUser', function () {
+        var user_id = $(this).data("id");
+        if(confirm("Are You sure want to delete !"))
+        {
+            $.ajax({
+                type: "DELETE",
+                url: "{{ url('user/') }}"+'/'+user_id,
+                success: function (data) {
+                alertDanger("User is deleted");
+                    table.draw();
+                },
+                error: function (data) {
+                    console.log('Error:', data);
+                }
+            });
+        }
     });
 });
 </script>
